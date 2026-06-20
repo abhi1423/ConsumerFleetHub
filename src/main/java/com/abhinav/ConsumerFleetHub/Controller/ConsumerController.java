b63ed632ef8375b34544d39e470de2a023a164b0
Abhinav Chandel <abnvchndel@gmail.com>
2026-06-20 23:06:30 +0530

    Added Authorization checks

--- src/main/java/com/abhinav/ConsumerFleetHub/Controller/ConsumerController.java
+++ src/main/java/com/abhinav/ConsumerFleetHub/Controller/ConsumerController.java
@@ -4,14 +4,17 @@ import com.abhinav.ConsumerFleetHub.DTOs.EndTrip;
 import com.abhinav.ConsumerFleetHub.DTOs.ResponseFomTransporter;
 import com.abhinav.ConsumerFleetHub.Entities.Consumer;
 import com.abhinav.ConsumerFleetHub.Entities.LoadQuery;
+import com.abhinav.ConsumerFleetHub.Entities.Role;
 import com.abhinav.ConsumerFleetHub.ResponseDTOs.Vehicle;
 import com.abhinav.ConsumerFleetHub.Services.ConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/consumer")
@@ -29,16 +32,28 @@ public class ConsumerController
 	@PostMapping("/add")
 	public ResponseEntity<Consumer> saveUser(@RequestBody Consumer consumer)
 	{
-		Consumer c=consumerService.saveUser(consumer);
+		Consumer c=consumerService.saveUser(consumer,"CONSUMER");
 		return new ResponseEntity<Consumer>(c,HttpStatus.CREATED);
 	}
+    @PostMapping("/add/admin")
+    public ResponseEntity<Consumer> saveUserAsAdmin(@RequestBody Consumer consumer)
+    {
+        Consumer c=consumerService.saveUser(consumer,"ADMIN");
+        Set<Role> roles = c.getRoles();
+        roles.clear();
+
+
+        return new ResponseEntity<Consumer>(c,HttpStatus.CREATED);
+    }
 	@PutMapping("/remove/query/{username}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('CONSUMER')")
 	public ResponseEntity<Consumer> removeQuery(@PathVariable String username)
 	{
 		Consumer c = consumerService.removeLoadQuery(username);
 		return new ResponseEntity<Consumer>(c,HttpStatus.OK);
 		
 	}
+    @PreAuthorize("hasRole('ADMIN') or hasRole('CONSUMER')")
 	@PutMapping("/create/query/{username}") 
 	public ResponseEntity<List<Vehicle>> createLoadQuery(@PathVariable String username,@RequestBody LoadQuery lq)
 	{
@@ -46,38 +61,46 @@ public class ConsumerController
 		return c;
 		
 	}
+    @PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/get/all")
 	public ResponseEntity<List<Consumer>> getAllConsumer()
 	{
 		List<Consumer> listConsumers=consumerService.getAllCosumers();
 		return new ResponseEntity<List<Consumer>>(listConsumers,HttpStatus.OK);
 	}
-	
+    @PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/get/all/pendingQueries")
 	public ResponseEntity<List<LoadQuery>> getAllQueries()
 	{
 		List<LoadQuery> listConsumers=consumerService.getAllPendigsQueries();
 		return new ResponseEntity<List<LoadQuery>>(listConsumers,HttpStatus.OK);
 	}
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('CONSUMER')")
     @GetMapping("/get/{username}")
 	public ResponseEntity<Consumer> getByUsername(String username)
 	{
     	Consumer c = consumerService.getConsumer(username);
     	return new ResponseEntity<Consumer>(c,HttpStatus.OK);
 	}
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('CONSUMER')")
     @GetMapping("/get/consumers/pendingQueries")
-    public ResponseEntity<List<Consumer>> getUserswithPendingQueries()
+    public ResponseEntity<List<Consumer>> getUsersWithPendingQueries()
     {
     	List<Consumer> list = consumerService.getAllCosumersWithPendingQueries();
     	return new ResponseEntity<List<Consumer>>(list,HttpStatus.OK);
     }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('CONSUMER')")
     @GetMapping("/getVehicle/{userId}/{vehicle_number}")
-    public ResponseEntity<Vehicle> postVehicleReuest(@PathVariable String userId,@PathVariable String vehicle_number)
+    public ResponseEntity<Vehicle> postVehicleRequest(@PathVariable String userId,@PathVariable String vehicle_number)
     {
     	return consumerService.bookMyVehicle(vehicle_number, userId);
     }
+    
     @PutMapping("/get/responseFromTransporter")
-    public ResponseEntity<ResponseFomTransporter> updateRequetStatus(@RequestBody ResponseFomTransporter responseFomTransporter)
+    public ResponseEntity<ResponseFomTransporter> updateRequestStatus(@RequestBody ResponseFomTransporter responseFomTransporter)
     {
     	System.out.println("The response from transporter is "+responseFomTransporter.isResponse());
     	ResponseFomTransporter response=consumerService.updateRequetStatus(responseFomTransporter);
--- src/main/java/com/abhinav/ConsumerFleetHub/configuration/SecurityConfiguration.java
+++ src/main/java/com/abhinav/ConsumerFleetHub/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Autowired
@@ -30,7 +32,6 @@ public class SecurityConfiguration {
                         auth
                                 .requestMatchers("/login/**").permitAll()
                                 .requestMatchers("/consumer/add").permitAll()
-                                .requestMatchers("/consumer/get/all").hasRole("CONSUMER")
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
